1a0c8f76baaba6415476717a362bfa7f1ce881cf
bogind <dror@kaplanopensource.co.il>
2022-01-17 23:07:13 +0200

    added some layer interactions

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-var map = L.map('map',{
+let map = L.map('map',{
     layers: [OpenStreetMap_Mapnik],
 
 }).setView([32.055572,34.756429], 13);
+
+let sets = {
+    "type": "FeatureCollection",
+    "features": []
+  }
+
+let setsLayer = L.geoJSON(sets, { 
+    style: defaultStyle,
+    onEachFeature: onEachFeature
+})
+
 
 mapInit()
 
--- util_functions.js
+++ util_functions.js
@@ -1,3 +1,10 @@
+const defaultStyle = { 
+    color: 'blue', 
+    weight: 2, 
+    fillOpacity: 0.2,
+};
+
+
 async function mapInit(){
 
     /*function handleHeaderMeta(headerMeta) {
@@ -8,36 +15,66 @@ async function mapInit(){
 
     const response = await fetch('set.fgb');
     for await (let feature of flatgeobuf.deserialize(response.body, undefined)) {
+        sets.features.push(feature)
         // Leaflet styling
-        const defaultStyle = { 
-            color: 'blue', 
-            weight: 2, 
-            fillOpacity: 0.2,
-        };
+        map.removeLayer(setsLayer)
+        setsLayer.addData(feature)
 
         // Add the feature to the map
-        L.geoJSON(feature, { 
-            style: defaultStyle 
-        }).on({
-            // highlight on hover
-            'mouseover': function(e) {
-                const layer = e.target;
-                layer.setStyle({
-                    color: 'blue',
-                    weight: 4,
-                    fillOpacity: 0.7,
-                });
-                layer.bringToFront();
-            },
-            // remove highlight when hover stops
-            'mouseout': function(e) {
-                const layer = e.target;
-                layer.setStyle(defaultStyle);
-            }
-        })
-        // show some per-feature properties when clicking on the feature
-        .bindPopup(`<h1>${feature.properties["LocNameHeb"]}</h1>`)
-        .addTo(map);
+        /*setsLayer = L.geoJSON(sets, { 
+            style: defaultStyle,
+            onEachFeature: onEachFeature
+        })*/
+        //.addTo(map);
+        if(sets.features.length == 406){
+            setsLayer.addTo(map);
+        }
     }
 
+}
+function onEachFeature(feature, layer){
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: onFeatureClick
+    });
+}
+function onFeatureClick(e){
+    let layer = e.target;
+    let feature = e.target.feature;
+    
+    if (feature.properties && feature.properties.LocNameHeb && feature.properties.LocalityCo) {
+        layer.bindPopup(`<h1>${feature.properties["LocNameHeb"]}</h1>\
+        <center>\
+        <h2>בגוש דן?</h2>\
+        <button onclick="sendYes(${feature.properties["LocalityCo"]})">כן</button>\
+        <button onclick="sendNo(${feature.properties["LocalityCo"]})">לא</button>\
+        </center>`)
+    }
+}
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        color: 'blue',
+        weight: 4,
+        fillOpacity: 0.7,
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+function resetHighlight(e) {
+    setsLayer.resetStyle(e.target);
+}
+function sendYes(code){
+    //need to create the app script API for this to do something
+}
+function sendNo(code){
+    //need to create the app script API for this to do something
+}
+function getRandomFetaure(){
+    let i = Math.floor(Math.random() * 406);
+    
 }
