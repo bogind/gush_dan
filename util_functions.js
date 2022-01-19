b3e6716948cdfab073ccf845549443c24ae4d5d3
bogind <dror@kaplanopensource.co.il>
2022-01-19 21:42:58 +0200

    added results functions can now show results, need UI

--- util_functions.js
+++ util_functions.js
@@ -1,9 +1,11 @@
 const defaultStyle = { 
-    color: 'blue', 
+    fillColor: 'blue', 
+    color: 'blue',
     weight: 2, 
     fillOpacity: 0.2,
 };
-const apiUrl = "https://script.google.com/macros/s/AKfycbzqHIiQ74HoUlADdX0LcNxPxC2qzNTse93QEsg8sPeg0y-w8i3opdFTD_gER8gXn78Y/exec"
+const apiUrl = "https://script.google.com/macros/s/AKfycbzqHIiQ74HoUlADdX0LcNxPxC2qzNTse93QEsg8sPeg0y-w8i3opdFTD_gER8gXn78Y/exec";
+let resultsLayer;
 
 async function mapInit(){
 
@@ -48,6 +50,28 @@ function onEachFeature(feature, layer){
         click: onFeatureClick
     });
 }
+function onEachResultsFeature(feature, layer){
+    if (feature.properties && feature.properties.LocNameHeb && feature.properties.LocalityCo) {
+        result = 'ללא הכרעה'
+        if(feature.properties.yes > feature.properties.no){
+            result = "חלק מגוש דן"
+        }else if(feature.properties.yes < feature.properties.no){
+            result = "לא חלק מגוש דן"
+        }
+
+        layer.bindPopup(`<h1>${feature.properties["LocNameHeb"]}</h1>\
+        <center>\
+        <h2>החלטת הקהל: ${result}</h2>
+        <h3><b>בעד:</b> ${feature.properties.yes}<br></h3>\
+        <h3><b>נגד:</b> ${feature.properties.no}<br></h3>\
+        </center>`)
+    }
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: onFeatureClick
+    });
+}
 function onFeatureClick(e){
     let layer = e.target;
     let feature = e.target.feature;
@@ -57,9 +81,9 @@ function highlightFeature(e) {
     var layer = e.target;
 
     layer.setStyle({
-        color: 'blue',
+        color: 'cyan',
         weight: 4,
-        fillOpacity: 0.7,
+        //fillOpacity: 0.7,
     });
 
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
@@ -67,7 +91,12 @@ function highlightFeature(e) {
     }
 }
 function resetHighlight(e) {
-    setsLayer.resetStyle(e.target);
+    
+    if(map.hasLayer(setsLayer)){
+        setsLayer.resetStyle(e.target);
+    }else{
+        resultsLayer.resetStyle(e.target);
+    };
 }
 function sendYes(code){
     fetch(apiUrl+`?code=${code}&answer=1`, {
@@ -121,4 +150,48 @@ function getRandomFetaure(){
         
     })
     
+}
+function showResults(){
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => joinAttributes(data))
+}
+function joinAttributes(results){
+    
+    sets.features.forEach(feature =>{
+        for(r=0;r<results.features.length;r++){
+            row = results.features[r]
+            if(row.id === feature.properties.LocalityCo){
+                feature.properties['yes'] = row.properties.yes
+                feature.properties['no'] = row.properties.no
+                feature.properties.color = getColor(feature.properties)
+            }
+        }
+    })
+    if(map.hasLayer(setsLayer)){map.removeLayer(setsLayer)}
+    resultsLayer = L.geoJSON(sets, { 
+        style: getFeatureStyle,
+        onEachFeature: onEachResultsFeature,
+        attribution: 'גבולות: למ"ס 2013, תוצאות: אתם'
+    })
+    resultsLayer.addTo(map)
+
+}
+function getFeatureStyle(feature){
+    return {
+        fillColor: feature.properties.color,
+        weight: 2,
+        opacity: 0.2,
+        color: 'gray',
+        fillOpacity: 0.4
+    };
+}
+function getColor(properties){
+    let color = "#d95f02"
+    if(properties.yes > properties.no){
+        color = "#1b9e77"
+    }else if(properties.yes < properties.no){
+        color = "#7570b3"
+    }
+    return color;
 }
